08cc30a05176cd87c8e964ec746a5cd311995a4c
Quintin <quintinsmith@icloud.com>
2026-04-20 22:43:08 -0600

    Initial Commit

---
+++ lifting/liftingApp.swift
@@ -0,0 +1,32 @@
+//
+//  liftingApp.swift
+//  lifting
+//
+//  Created by Quintin Smith on 4/20/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct liftingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
